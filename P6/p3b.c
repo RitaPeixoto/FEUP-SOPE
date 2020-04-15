484f1dd7ef352c3cb934fa511f21c6d24d929f5f
Rita <rita.peixoto2000@gmail.com>
2020-04-15 09:41:06 +0100

    Exercise 3 done

--- P6/p3b.c
+++ P6/p3b.c
@@ -13,9 +13,8 @@ int main() {
     int t;    
     for(t=0; t< NUM_THREADS; t++){       
         printf("Creating thread %d\n", t);       
-        pthread_create(&threads[t], NULL, PrintHello, (void *)&t);    
+        pthread_create(&threads[t], NULL, PrintHello, (void *)&t);  
+        pthread_join(threads[t],NULL);  
     }    
     pthread_exit(0); 
 }
-// O resultado nao é o esperado porque não há nada que fique à espera que o thread anterior termine e portanto
-//printe o hello from thread...

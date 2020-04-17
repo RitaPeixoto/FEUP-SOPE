e49874c09cc23a4ebca4441b1fa7c69a1836c8bf
Rita <rita.peixoto2000@gmail.com>
2020-04-17 16:45:55 +0100

    Only exercise 9 isn't done

---
+++ P5/p9_client.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/file.h>
+#include <errno.h>
+#include <string.h>
+
+
+
+/*O cliente simplesmente cria o FIFO para receber a resposta, envia o seu pid para o
+servidor e o comando para este executar, espera pela resposta e, quando esta chegar, mostra‐a no écran e
+termina.*/
+
+
+int main(){
+
+
+
+
+    return 0;
+}
---
+++ P5/p9_server.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/file.h>
+#include <errno.h>
+#include <string.h>
+#include <wait.h>
+
+
+
+
+
+int main(){
+
+
+
+
+    return 0;
+}

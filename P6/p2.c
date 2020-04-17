670d30c5b0b30fff81d18fc4d590c639f08442e1
Rita <rita.peixoto2000@gmail.com>
2020-04-17 12:25:02 +0100

    Finished exercise 2

--- P6/p2.c
+++ P6/p2.c
@@ -6,32 +6,38 @@
 #define STDERR 2 
 #define NUMITER 10000 
 
-int N = 50000;
+int N;
 
-void * thrfunc(void * arg) {   
-    int i=0;   
-    fprintf(stderr, "Starting thread %s\n", (char *) arg); 
-    while(N>0){
-        write(STDERR,arg,1);
+void * thrfunc(void * arg) {  
+    int i = 0;
+    int value = *(int*) arg;
+    char s[5];
+    sprintf(s, "%d", value);
+    fprintf(stderr, "Starting thread %d\n", value);
+    while(N > 1) {
+        write(STDERR, s, 1);
         i++;
         N--;
     }
-     void * res = malloc(sizeof(int));
-    *(int *)res = i;
-    return res;
+    void* res = malloc(sizeof(int));
+    *(int *) res = i;
+    return res; 
 } 
-int main() {    
+int main() {  
+    N=5000;  
     pthread_t ta, tb;
-    void *t1, *t2;   
+    void *t1, *t2; 
+    int num[2]={1,2};  
 
-    pthread_create(&ta, NULL, thrfunc, "1");   
-    pthread_create(&tb, NULL, thrfunc, "2"); 
+    pthread_create(&ta, NULL, thrfunc, &num[0]);   
+    pthread_create(&tb, NULL, thrfunc, &num[1]); 
 
     pthread_join(ta, &t1);   
     pthread_join(tb, &t2); 
 
-    fprintf(stderr,"\nThread 1 wrote %d characters\n",*(int*)ta);
-    fprintf(stderr,"\nThread 2 wrote %d characters\n",*(int*)tb);
-
+    fprintf(stderr,"\nThread 1 wrote %d characters\n",*(int*)t1);
+    fprintf(stderr,"\nThread 2 wrote %d characters\n",*(int*)t2);
+    free(t1);
+    free(t2);
     return 0; 
 }

19ecb5af63041c579dd6deb7bcb6dc06d5c668f9
Rita <rita.peixoto2000@gmail.com>
2020-04-17 15:47:59 +0100

    Exercise 6 done

--- P5/p6_writer.c
+++ P5/p6_writer.c
@@ -1,10 +1,12 @@
 // PROGRAMA p06_writer.c 
-#include<sys/types.h>
-#include<unistd.h>
+#include <sys/types.h>
+#include <unistd.h>
 #include <sys/stat.h>
-#include<stdio.h>
-#include<fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
 #include <string.h>
+
 int main(void) {  
     int   fd, messagelen, i;  
     char  message[100];  
@@ -12,13 +14,16 @@ int main(void) {
     do {    
         fd=open("/tmp/myfifo",O_WRONLY);    
         if (fd==-1) sleep(1);  
-    } while (fd==-1);  
+    } while (fd==-1); 
+
     for (i=1; i<=3; i++) {    
         sprintf(message,"Hello no. %d from process %d\n", i, getpid());    
         messagelen=strlen(message)+1;    
         write(fd,message,messagelen);    
         sleep(3);   
     }  
-    close(fd);  
+
+    close(fd);
+
     return 0; 
 } 
